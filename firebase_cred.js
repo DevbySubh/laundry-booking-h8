5948675bd075827c2e78cec4b5c279e66856e625
sappy <70689030+DevbySubh@users.noreply.github.com>
2025-07-19 01:42:06 +0530

    Create firebase_cred.js

---
+++ firebase_cred.js
@@ -0,0 +1,8 @@
+{     apiKey: "AIzaSyBfU-aqLYHF_X0Rwv0WqZtZB_M0cdacoDg",
+      authDomain: "launry-booking-h8.firebaseapp.com",
+      databaseURL: "https://launry-booking-h8-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "launry-booking-h8",
+      storageBucket: "launry-booking-h8.appspot.com",
+      messagingSenderId: "485927717258",
+      appId: "1:485927717258:web:be15abc9f8cd281846e073"
+    };
